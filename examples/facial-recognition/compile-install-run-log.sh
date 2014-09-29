fbfafbca7d59334be586ae0a00ae54954d46b566
Brandon Amos <bamos@cmu.edu>
2014-09-28 20:06:10 -0400

    Remove scripts from FacialRecognition example.

--- examples/facial-recognition/compile-install-run-log.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-set -x
-PKG='edu.cmu.cs.encoding'
-ACTIVITY='EncodingBenchmarkActivity'
-ant debug &> ant.log || exit 1
-adb uninstall $PKG
-adb install bin/$ACTIVITY-debug.apk &> adb-install.log || exit 3
-adb shell am start -n $PKG/.$ACTIVITY || exit 4
-adb logcat | grep 'Gabriel'
