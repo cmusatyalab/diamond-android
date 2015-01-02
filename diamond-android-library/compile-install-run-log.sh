ec33f5dea86231dc86ffb9d7c2d6b69110e70c31
Brandon Amos <bamos@cmu.edu>
2015-01-01 21:40:32 -0500

    Add license to all files for #19.

--- diamond-android-library/compile-install-run-log.sh
+++ diamond-android-library/compile-install-run-log.sh
@@ -1,4 +1,15 @@
-#/bin/bash
+#!/bin/bash
+#
+#  Diamond Android - Diamond filters for the Android platform
+#
+#  Copyright (c) 2013-2014 Carnegie Mellon University
+#  All Rights Reserved.
+#
+#  This software is distributed under the terms of the Eclipse Public
+#  License, Version 1.0 which can be found in the file named LICENSE.
+#  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+#  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+
 set -x
 PKG='edu.cmu.cs.encoding'
 ACTIVITY='EncodingBenchmarkActivity'
